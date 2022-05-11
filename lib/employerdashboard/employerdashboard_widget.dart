45884e91c2f49ccd68166065df0cfebc1499bad2
bjornskaarsfjord <bjorn.skaarsfjord@gmail.com>
2022-05-11 19:28:47 -0400

    added charts and cleaned up homepage

--- lib/employerdashboard/employerdashboard_widget.dart
+++ lib/employerdashboard/employerdashboard_widget.dart
@@ -1,3 +1,5 @@
+import 'package:payroll/simplebarchart/simplebarchart.dart';
+
 import '../flutter_flow/flutter_flow_drop_down.dart';
 import '../flutter_flow/flutter_flow_theme.dart';
 import '../flutter_flow/flutter_flow_util.dart';
@@ -5,6 +7,9 @@ import '../appbars/employer_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pie_chart/pie_chart.dart';
+
+import '../flutter_flow/flutter_flow_widgets.dart';
+
 
 class EmployerdashboardWidget extends StatefulWidget {
   const EmployerdashboardWidget({Key key}) : super(key: key);
@@ -37,138 +42,228 @@ class _EmployerdashboardWidgetState extends State<EmployerdashboardWidget> {
       body: SafeArea(
         child: GestureDetector(
           onTap: () => FocusScope.of(context).unfocus(),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Row(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                        child: Text(
+                          'Next payday: June 9, 2022',
+                          textAlign: TextAlign.center,
+                          style: FlutterFlowTheme.of(context).bodyText1.override(
+                                fontFamily: 'Poppins',
+                                fontSize: 20,
+                              ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
                 mainAxisSize: MainAxisSize.max,
                 children: [
                   Expanded(
                     child: Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 20),
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
                       child: Text(
-                        'Next payday: Dec 19, 2022',
+                        '14,056.90 UST',
                         textAlign: TextAlign.center,
                         style: FlutterFlowTheme.of(context).bodyText1.override(
                               fontFamily: 'Poppins',
-                              fontSize: 20,
+                              fontSize: 50,
                             ),
                       ),
                     ),
                   ),
                 ],
               ),
-              Row(
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 30),
+                child: Row(
                 mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                
                 children: [
-                  FlutterFlowDropDown(
-                    initialOption: dropDownValue1 ??= 'last 3 months',
-                    options: [
-                      'last month',
-                      'last 3 months',
-                      'last 6 months',
-                      'last 12 months'
-                    ].toList(),
-                    onChanged: (val) => setState(() => dropDownValue1 = val),
-                    width: 180,
-                    height: 50,
-                    textStyle: FlutterFlowTheme.of(context).bodyText1.override(
-                          fontFamily: 'Poppins',
-                          color: Colors.black,
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 5, 0),
+                    child: FFButtonWidget(
+                      onPressed: () {
+                        print('Button pressed ...');
+                      },
+                      text: 'Add',
+                      options: FFButtonOptions(
+                        width: 130,
+                        height: 40,
+                        color: FlutterFlowTheme.of(context).primaryColor,
+                        textStyle:
+                            FlutterFlowTheme.of(context).subtitle2.override(
+                                  fontFamily: 'Poppins',
+                                  color: Colors.white,
+                                ),
+                        borderSide: BorderSide(
+                          color: Colors.transparent,
+                          width: 1,
                         ),
-                    fillColor: Colors.white,
-                    elevation: 2,
-                    borderColor: Colors.transparent,
-                    borderWidth: 0,
-                    borderRadius: 0,
-                    margin: EdgeInsetsDirectional.fromSTEB(12, 4, 12, 4),
-                    hidesUnderline: true,
+                        borderRadius: 12,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(5, 0, 0, 0),
+                    child: FFButtonWidget(
+                      onPressed: () {
+                        print('Button pressed ...');
+                      },
+                      text: 'Remove',
+                      options: FFButtonOptions(
+                        width: 130,
+                        height: 40,
+                        color: FlutterFlowTheme.of(context).primaryColor,
+                        textStyle:
+                            FlutterFlowTheme.of(context).subtitle2.override(
+                                  fontFamily: 'Poppins',
+                                  color: Colors.white,
+                                ),
+                        borderSide: BorderSide(
+                          color: Colors.transparent,
+                          width: 1,
+                        ),
+                        borderRadius: 12,
+                      ),
+                    ),
                   ),
                 ],
               ),
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 0),
-                child: Row(
+              ),
+
+                Row(
                   mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text(
-                      'TODO: add chart',
-                      style: FlutterFlowTheme.of(context).bodyText1,
+                    FlutterFlowDropDown(
+                      initialOption: dropDownValue1 ??= 'last 3 months',
+                      options: [
+                        'last month',
+                        'last 3 months',
+                        'last 6 months',
+                        'last 12 months'
+                      ].toList(),
+                      onChanged: (val) => setState(() => dropDownValue1 = val),
+                      width: 180,
+                      height: 50,
+                      textStyle: FlutterFlowTheme.of(context).bodyText1.override(
+                            fontFamily: 'Poppins',
+                            color: Colors.black,
+                          ),
+                      fillColor: Colors.white,
+                      elevation: 2,
+                      borderColor: Colors.transparent,
+                      borderWidth: 0,
+                      borderRadius: 0,
+                      margin: EdgeInsetsDirectional.fromSTEB(12, 4, 12, 4),
+                      hidesUnderline: true,
                     ),
                   ],
                 ),
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  FlutterFlowDropDown(
-                    initialOption: dropDownValue2 ??= 'last payday',
-                    options: [
-                      'last payday',
-                      'last 2 paydays',
-                      'last 4 paydays',
-                      'last 8 paydays'
-                    ].toList(),
-                    onChanged: (val) => setState(() => dropDownValue2 = val),
-                    width: 180,
-                    height: 50,
-                    textStyle: FlutterFlowTheme.of(context).bodyText1.override(
-                          fontFamily: 'Poppins',
-                          color: Colors.black,
-                        ),
-                    fillColor: Colors.white,
-                    elevation: 2,
-                    borderColor: Colors.transparent,
-                    borderWidth: 0,
-                    borderRadius: 0,
-                    margin: EdgeInsetsDirectional.fromSTEB(12, 4, 12, 4),
-                    hidesUnderline: true,
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 10),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child:SimpleBarChart.withSampleData(),
+                        height: 200,
+                        width: 400,
+                      )
+                      
+                    ],
                   ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 0),
-                child: Row(
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(8, 0, 0, 0),
+                  child: Row(
                   mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Container(
-                      child: PieChart(
-                        dataMap: dataMap,
-                        animationDuration: Duration(milliseconds: 800),
-                        chartLegendSpacing: 32,
-                        chartRadius: 200,
-                        //colorList: colorList,
-                        initialAngleInDegree: 0,
-                        chartType: ChartType.disc,
-                        ringStrokeWidth: 32,
-                        //centerText: "HYBRID",
-                        legendOptions: LegendOptions(
-                          showLegendsInRow: false,
-                          legendPosition: LegendPosition.bottom,
-                          showLegends: true,
-                          //legendShape: _BoxShape.circle,
-                          legendTextStyle: TextStyle(
-                            fontWeight: FontWeight.bold,
+                    FlutterFlowDropDown(
+                      initialOption: dropDownValue2 ??= 'last payday',
+                      options: [
+                        'last payday',
+                        'last 2 paydays',
+                        'last 4 paydays',
+                        'last 8 paydays'
+                      ].toList(),
+                      onChanged: (val) => setState(() => dropDownValue2 = val),
+                      width: 180,
+                      height: 50,
+                      textStyle: FlutterFlowTheme.of(context).bodyText1.override(
+                            fontFamily: 'Poppins',
+                            color: Colors.black,
                           ),
-                        ),
-                        chartValuesOptions: ChartValuesOptions(
-                          showChartValueBackground: true,
-                          showChartValues: true,
-                          showChartValuesInPercentage: false,
-                          showChartValuesOutside: false,
-                          decimalPlaces: 2,
-                        ),
-                        // gradientList: ---To add gradient colors---
-                        // emptyColorGradient: ---Empty Color gradient---
-                      ),
-                      height: 400,
-                      width: 400,
-                    )
-                    
+                      fillColor: Colors.white,
+                      elevation: 2,
+                      borderColor: Colors.transparent,
+                      borderWidth: 0,
+                      borderRadius: 0,
+                      margin: EdgeInsetsDirectional.fromSTEB(12, 4, 12, 4),
+                      hidesUnderline: true,
+                    ),
                   ],
                 ),
-              ),
-            ],
+                ),
+                Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 20),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        child: PieChart(
+                          dataMap: dataMap,
+                          animationDuration: Duration(milliseconds: 800),
+                          chartLegendSpacing: 32,
+                          chartRadius: 200,
+                          //colorList: colorList,
+                          initialAngleInDegree: 0,
+                          chartType: ChartType.disc,
+                          ringStrokeWidth: 32,
+                          //centerText: "HYBRID",
+                          legendOptions: LegendOptions(
+                            showLegendsInRow: false,
+                            legendPosition: LegendPosition.bottom,
+                            showLegends: true,
+                            //legendShape: _BoxShape.circle,
+                            legendTextStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          chartValuesOptions: ChartValuesOptions(
+                            showChartValueBackground: true,
+                            showChartValues: true,
+                            showChartValuesInPercentage: false,
+                            showChartValuesOutside: false,
+                            decimalPlaces: 2,
+                          ),
+                          // gradientList: ---To add gradient colors---
+                          // emptyColorGradient: ---Empty Color gradient---
+                        ),
+                        height: 300,
+                        width: 400,
+                      )
+                      
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
